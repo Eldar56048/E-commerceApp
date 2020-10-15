44d9d860068867e6b2dc4ce8bffd32942910cc8f
Eldar56048 <sayrambayeldar12@gmail.com>
2020-10-15 20:08:04 +0600

    new class Functions. and add wishlist.jsp

---
+++ src/com/company/models/Functions.java
@@ -0,0 +1,45 @@
+package com.company.models;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Functions {
+    public int countSizeCart(ArrayList<Item> items){
+        int size =0;
+        for (Item item:items){
+            size+=item.getQuantity();
+        }
+        return size;
+    }
+
+    public int countWishCartSize(ArrayList<Product> wishList){
+        return wishList.size();
+    }
+
+    public boolean isWishInCart(ArrayList<Product> products, Product product){
+        for(Product product1 : products) {
+            if(product1.getId()==product.getId())return true;
+        }
+        return false;
+    }
+
+    public boolean isItemInCart(ArrayList<Item> items, Product product){
+        for(Item item : items) {
+            if(item.getProduct().getId()==product.getId())return true;
+        }
+        return false;
+    }
+
+    public ArrayList<Product> getCategoryProducts(ArrayList<Product> products,String category){
+        ArrayList<Product> categoryProducts = new ArrayList<>();
+        for(Product product: products){
+            if(product.getCategory().equals(category)){
+                categoryProducts.add(product);
+            }
+        }
+        return categoryProducts;
+    }
+
+
+
+}
